49be1b5b52b6cc53d378f2be930d83ea328da3cf
q1ra <q1ra@nykredit.dk>
2018-06-04 16:35:30 +0200

    Build tweaks

--- karma.conf.js
+++ karma.conf.js
@@ -6,30 +6,17 @@ module.exports = (config) => {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-coverage-istanbul-reporter'),
-      require('karma-junit-reporter'),
       require('karma-mocha-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    preprocessors: {
-      
-    },
-    angularCli: {
-      environment: 'dev'
-    },
-    reporters: ['mocha', 'junit', 'coverage-istanbul'],
-    junitReporter: {
-      outputDir: 'target',
-      outputFile: 'test-results.xml',
-      suite: '',
-      useBrowserName: false
-    },
+    reporters: ['mocha', 'coverage-istanbul'],
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: ['cobertura'],
+      dir: require('path').join(__dirname, 'coverage'), reports: ['html'],
       fixWebpackSourcePaths: true,
-      dir: 'target'
+      dir: 'target/coverage'
     },
     port: 9876,
     colors: true,
